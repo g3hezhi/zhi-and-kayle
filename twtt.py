1042bab335105bb50115c272ebadf295f4e0d40e
kidkile <kidkile@gmail.com>
2016-01-22 10:15:29 -0500

    Update twtt.py

--- twtt.py
+++ twtt.py
@@ -1,18 +1,19 @@
 import sys
 
-# read tweet corpus and put it on the array
-array = []
-f = open(sys.argv[1],'r')
-for line in f:
+def filter(file):
+	''' file -> array of string '''
+	array = []
+	for line in file:
 	# 1. remove html tags
-	for ch in ['/<','>/','</','/>']:
-		line = line.replace(ch,"")
-	# 2. replace html char with ASCII
-	for ch in ['/<','>/','</','/>']:
-		line = line.replace(ch,"")
-	# 3.
-	for ch in ['/<','>/','</','/>']:
-		line = line.replace(ch,"")
+		for ch in ['/<','>/','</','/>']:
+			line = line.replace(ch,"")
 	array.append(line)
-print(array)
+	print(array)
+
+# computing filter function
+if __name__ == "__main__":
+	# read tweet corpus and put it on the array	
+	f = open(sys.argv[1],'r')
+	filter(f)
+
 
