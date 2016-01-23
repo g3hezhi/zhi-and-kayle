3d003ac8fe5e2581f266d7dbf36b6dca5f7ca399
zhiwei he <hezhiweitian@gmail.com>
2016-01-23 12:06:35 -0500

    update general structure of the main program and task 1

--- twtt.py
+++ twtt.py
@@ -1,5 +1,26 @@
 import sys
 
+#### write help function that only take string/list as input, and output desired processed string##
+
+
+def task1(twtText):
+	'''
+	input = st = "Meet me today at the FEC in DC at 4. Wear a carnation so I know it¡¯s you. \d href=Http://bit.ly/PACattack <a href=Http://bit.ly/PACattack ?> + ^ g"
+	output = 'Meet me today at the FEC in DC at 4. Wear a carnation so I know it¡¯s you. g'
+	'''
+	twtList = twtText.split()
+	print(twtList)
+	solu =""
+	for i in twtList:
+		if "<" in i or ">" in i or "/" in i or "\\" in i or "[" in i or "+" in i or "]" in i or "^" in i:
+			continue
+		else:
+			solu += i+" "
+	return solu.rstrip()
+				
+
+# we need to remove <\a> as a whole , not just the bracket. Your function will leave "a" which is a html tag.
+# after removing it, it become english word "a", which is not correct.
 def filter(file):
 	''' file -> array of string '''
 	array = []
@@ -10,10 +31,44 @@ def filter(file):
 	array.append(line)
 	print(array)
 
+
+
 # computing filter function
 if __name__ == "__main__":
-	# read tweet corpus and put it on the array	
-	f = open(sys.argv[1],'r')
-	filter(f)
+	#total = len(sys.argv)
+	#s = str(sys.argv)
+	filename = sys.argv[1]
+	groupID = sys.argv[2]
+	output = sys.argv[3]
+	filtered = []
+	outf = open(sys.argv[3],"w")
+	
+	# tokenize everyline in the filename into this:
+	#0 the polarity of the tweet (0 = negative emotion, 4 = positive emotion)
+	#1 the id of the tweet (e.g., 2087)
+	#2 the date of the tweet (e.g., Sat May 16 23:58:44 UTC 2009)
+	#3 the query (e.g., lyx). If there is no query, then this value is NO QUERY.
+	#4 the user that tweeted (e.g., robotickilldozr)
+	#5-beyond the text of the tweet (e.g., Lyx is cool)
+	# we now only process the text of the tweet. NOTE that twtText is a string.	
+	with open(filename) as f:
+		for line in f:
+			tokens = line.split(",")
+			twtText = ",".join(tokens[5:])
+			
+			
+			########## Now we just have to filter twtText, by calling the helper function#####
+			
+			## IMPLEMENT HERE ##
+			filtered.append(twtText)
+		for i in filtered:
+			outf.write(i+"\n")
+	outf.close()
+	f.close()
+	
+
+
+	
+		
 
 
